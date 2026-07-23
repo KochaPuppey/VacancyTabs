f87c74d526d9da4cef5911199a31559b912a270c
Andrew Kochkin <kochkin_9@mail.ru>
2026-07-23 08:15:48 +0300

    gh-pages6

--- src/main.tsx
+++ src/main.tsx
@@ -28,7 +28,7 @@ const theme = createTheme({
 });
 
 createRoot(document.getElementById('root')!).render(
-      <BrowserRouter   basename={import.meta.env.DEV ? "/" : "/VacancyUseMatch/"}>
+      <BrowserRouter   basename={import.meta.env.DEV ? "/" : "/VacancyTabs/"}>
       <Provider store = {store}>
       <MantineProvider theme={theme}>
     <App />
